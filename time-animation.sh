97f58d8cc3e32a3ff978f0c0e10aa467ca76e459
hug5 <dhub2@gmx.com>
2024-01-03 19:24:08 -0800

    add dynamic frequency hr/min

--- time-animation.sh
+++ time-animation.sh
@@ -8,7 +8,10 @@ set -u
 # Location : /shell-scripts/time-animation
 # Website  : xxxxx
 # What     : Decorative time display with some animation;
-#            Demonstrate animation without flicker;
+#            - Demonstrate animation without flicker;
+#            - Use arrays and random
+#            - Top/bottom glyphs' movements correspond to hour/min.
+#            -
 # License  : GPLv2
 # Author   : xxxx <xxx@xxxxx.com>
 
@@ -69,14 +72,17 @@ sec_unit=1
 
 # How long to sleep between clock microsecond updates
 # Divide sec_unit by sec_divisor to get the sleep period
-sec_divisor=20
-sleep_interv=$( echo "scale=3; $sec_unit/$sec_divisor" | bc )
+# sec_divisor=20
+sec_divisor=30
+sleep_interv=$( echo "scale=4; $sec_unit/$sec_divisor" | bc )
 
 adj_real_sec=$(( $sec_unit * $sec_divisor ))
 
 # 10/13 sets the animation movement; higher number, faster;
-ani_freqency[0]=$(( $adj_real_sec / 6 ))
-ani_freqency[1]=$(( $adj_real_sec / 10 ))
+# ani_freqency[0]=$(( $adj_real_sec / 6 ))
+# ani_freqency[1]=$(( $adj_real_sec / 10 ))
+ani_freqency[0]=9999
+ani_freqency[1]=9999
 
 
 # Animation track length
@@ -93,7 +99,6 @@ ani_direction[0]=1
 ani_direction[1]=1
 
 # The animation glyph
-
 ani_glyph[0]="▄▄▄▄"
 ani_glyph[1]="▀▀▀▀"
 
@@ -152,10 +157,36 @@ function set_spinner_glyph() {
     spinner_glyph=${spinner_glyph_arr[$rand_spinner_index]}
 }
 
+
+function set_hour_minute(){
+
+    local who_id=$1
+
+    if [[ $who_id -eq 0 ]]; then
+        local hr=$(( $( echo $(date +'%H') | bc) + 1 ))   # hour in 24hrs; +1 to prevent 0 hr;
+        # local hr=23
+        hr=$(echo "scale=2; $hr/2" | bc ) # scale to 2 decimal places; we want decimals;
+        ani_freqency[0]=$(echo "$adj_real_sec/$hr" | bc ) # No scaling; want integer only;
+    else
+        local mn=$(( $(echo $(date +'%M') | bc) + 1 ))   # minutes 00-59; +1 to prevent 0min
+        # local mn=30
+        mn=$(echo "scale=2; $mn/2" | bc )   # scale to 2 decimal places; we want decimals;
+        ani_freqency[1]=$(echo "$adj_real_sec/$mn" | bc ) # No scaling; want integer only;
+    fi
+
+    # I adjust the $hr and $mn variables, dividing by an adjustment factor,
+    # So that I can slow down the timing a bit;
+    # if number is 08 and try to do something, bash will interpret 08 as
+    # an octal and get error; run through bc to remove leading zero; I wanted to
+    # add 1 to prevent zero, but it's also causing the octal issue with 08;
+}
+
+
 # Check if the track is supposed to animate
 function check_frequency() {
 
     local who_id=$1
+    set_hour_minute $who_id
 
     if [[ ${loop_counter[$who_id]} -gt ${ani_freqency[$who_id]} ]]; then
 
@@ -191,6 +222,12 @@ printf '\e[?25l'
 
 # Clear screen
 clear
+
+# predraw our animation in case the draw frequncy is very low
+# and nothing draws for a while
+do_animation 0
+do_animation 1
+set_spinner_glyph
 
 # Changed from just true to using a variable == true because it seems
 # that when calling outside the file folder, the program won't exit on ctrl-c;
